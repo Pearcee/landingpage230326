a66253229fc0984a3d9f1cfbe4c9d9cfcef8d4b3
Steven Pearce <steven@waverley-housing.co.uk>
2023-03-24 13:18:04 +0000

    commited at 03/24/2023 13:18:04

---
+++ Git.ps1
@@ -0,0 +1,6 @@
+
+$comment = get-date
+git status
+git add --all .
+git commit -m "commited at $comment"
+git push -u origin main
---
+++ git.sh
@@ -0,0 +1,5 @@
+
+git status
+git add --all .
+git commit -m "commited at $comment"
+git push -u origin main
